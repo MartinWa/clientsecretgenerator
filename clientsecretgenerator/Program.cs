b74ae539ab6aad2a2227eb21df88d90d9384e52b
Martin Wåger <martin.w@comaround.se>
2016-06-16 21:47:00 +0200

    generator

---
+++ clientsecretgenerator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace clientsecretgenerator
+{
+    static class Program
+    {
+        private static void Main()
+        {
+            var guid = Guid.NewGuid().ToString();
+            var secret = guid.Sha256();
+            Console.WriteLine(guid);
+            Console.WriteLine(secret);
+        }
+    }
+
+    // Code taken from https://github.com/IdentityServer/IdentityServer3
+    internal static class Extensions
+    {
+        public static string Sha256(this string input)
+        {
+            if (input.IsMissing()) return string.Empty;
+
+            using (var sha = SHA256.Create())
+            {
+                var bytes = Encoding.UTF8.GetBytes(input);
+                var hash = sha.ComputeHash(bytes);
+
+                return Convert.ToBase64String(hash);
+            }
+        }
+
+        private static bool IsMissing(this string value)
+        {
+            return string.IsNullOrWhiteSpace(value);
+        }
+    }
+}
